0de1b7466d5b2c96db3f98fdaa8b8ea4b1d672fb
Nami Miyashiro <nami.miyashiro@gmail.com>
2022-11-21 14:09:17 -0800

    moving data to my branch

--- src/main/java/com/group2/finalproject/dao/iSellRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group2.finalproject.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.group2.finalproject.entity.item_table;
-
-public interface iSellRepository extends CrudRepository <item_table, Long>{
-  
-}
