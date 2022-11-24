977fab2040597ec71eb8b8d6c47772ea628f1f3a
Nami Miyashiro <nami.miyashiro@gmail.com>
2022-11-23 20:39:26 -0800

    ready for diploy to Heroku

--- src/main/java/com/group2/finalproject/dao/iCartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group2.finalproject.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.group2.finalproject.entity.Cart;
-
-public interface iCartRepository extends CrudRepository<Cart, Long>{
-
-}
