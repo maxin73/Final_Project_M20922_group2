21755bcc3c2c7caab1d553f6c0dc6a42138cac10
Nami Miyashiro <nami.miyashiro@gmail.com>
2022-11-24 07:25:34 -0800

    pushing my branch here

---
+++ src/main/java/com/group2/finalproject/entity/Sell.java
@@ -0,0 +1,67 @@
+package com.group2.finalproject.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Sell {
+  
+  //Fields
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  private String itemName;
+  private int price;
+  private String itemDescription;
+  //private String image;
+
+
+  //Constructor
+  public Sell (){
+  }
+
+  public Sell(String itemName, int price, String itemDescription) {
+    this.itemName = itemName;
+    this.price = price;
+    this.itemDescription = itemDescription;
+  }
+
+
+  //Getter and Setter
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getItemName() {
+    return itemName;
+  }
+
+  public void setItemName(String itemName) {
+    this.itemName = itemName;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  public String getItemDescription() {
+    return itemDescription;
+  }
+
+  public void setItemDescription(String itemDescription) {
+    this.itemDescription = itemDescription;
+  }
+
+}
